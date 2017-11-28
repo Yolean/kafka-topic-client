5dc918caceeb6f6e55f4e0e215d353a2540fcfe3
Staffan Olsson <staffan@repos.se>
2017-11-28 10:34:49 +0100

    We'll want to get topic declaration messages as their java representation

--- src/main/java/se/yolean/kafka/topic/mgmt/AdminSchemaUpdate.java
+++ src/main/java/se/yolean/kafka/topic/mgmt/AdminSchemaUpdate.java
@@ -5,18 +5,25 @@ import java.util.Collection;
 
 import javax.inject.Inject;
 
+import org.apache.avro.Schema;
+
 import com.github.structlog4j.ILogger;
 import com.github.structlog4j.SLoggerFactory;
 
 import io.confluent.kafka.schemaregistry.client.SchemaMetadata;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import se.yolean.kafka.topic.declaration.Topic;
 
 public class AdminSchemaUpdate {
 
   private ILogger log = SLoggerFactory.getLogger(this.getClass());
 
   private SchemaRegistryClient client;
+
+  private String topicDeclarationSchemaName = "topic_declaration";
+
+  private Class topicDeclarationGeneratedClass = Topic.class;
 
   @Inject
   public AdminSchemaUpdate(SchemaRegistryClient client) {
@@ -29,6 +36,7 @@ public class AdminSchemaUpdate {
       SchemaMetadata metadata = client.getLatestSchemaMetadata(subject);
       log.debug("Found schema", "subject", subject, "id", metadata.getId(), "version", metadata.getVersion());
       log.debug("" + metadata.getSchema());
+      Schema latestSchema = client.getBySubjectAndId(subject, metadata.getId());
     }
   }
 
